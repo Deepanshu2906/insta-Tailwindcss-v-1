a44993ee352267830ff7c4324c6b811adba4b04b
Deepanshu2906 <deepanshugoyal2906@gmail.com>
2023-02-02 15:34:28 +0530

    post section comments section creating

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,5 +4,5 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [require('@tailwindcss/forms'),require('tailwind-scrollbar')],
+  plugins: [require('@tailwindcss/forms'),require('tailwind-scrollbar',require('tailwindcss'))],
 }
